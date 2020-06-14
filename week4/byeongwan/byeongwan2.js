8c446f5c5aaeffcba8c510ec301f060b23fda01f
KimSatGat <cbaal@korea.com>
2020-06-14 16:52:28 +0900

    week5-byeongwan 1,2

--- week4/byeongwan/byeongwan2.js
+++ week4/byeongwan/byeongwan2.js
@@ -1,61 +1,4 @@
-// 1. 탑 
-function solution1(heights) {    
-    var answer = [];
-
-    while(heights.length){
-        let pivot = heights.pop();
-        let flag = false;
-        for(let i = heights.length; i >= 0; --i){
-            if(heights[i] > pivot){
-                answer.unshift(i + 1);
-                flag = true;
-                break;
-            }
-        }
-        if(!flag) answer.unshift(0);
-    }
-    return answer;
-}
-
-// 2. 멀쩡한 사각형 - X
-
-// 3. 다리를 지나는 트럭
-function solution3(bridge_length, weight, truck_weights) {
-    var sec = 0;
-    let runnigList = [];
-    let lenList = [];
-    let nowWeight = 0;
-    while(runnigList.length || truck_weights.length){
-        let count = 0;  // 빠져 나간 트럭 수
-        // 1초 증가
-        ++sec;
-        // runningList가 비어 있거나 무게가 견딜 수 있다면 대기 트럭 추가
-        if(!runnigList.length || weight >= nowWeight + truck_weights[0]){
-            nowWeight += truck_weights[0];
-            runnigList.push(truck_weights.shift());
-            lenList.push(bridge_length);
-        }
-
-        // 남은 다리 길이 감소
-        for(let i = 0; i < runnigList.length; ++i){
-            --lenList[i];
-            if(lenList[i] <= 0){
-                ++count;
-                nowWeight -= runnigList[i]
-            }
-        }
-
-        // 다리 위 트럭 제거
-        runnigList.splice(0, count);
-        lenList.splice(0, count);
-    }    
-    return sec + 1;  
-}
-
-// 4. 숫자야구 - X
-
-// 5. 실패율 - X
-function solution5(N, stages) {
+function solution(N, stages) {
     var answer = [];
     let people = stages.length;
     let successList = new Array(N + 1).fill(0);
@@ -86,31 +29,13 @@ function solution5(N, stages) {
     }
     
     // 맵 내림차순 정렬
-    let sortedMap = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+    let sortedMap = new Map([...map.entries()].sort((a, b) =>{
+        if(a[0] === b[0]){
+            a[0] - b[0];
+        }else{
+            b[1] - a[1];
+        }        
+    }))
     for(let key of sortedMap.keys()) answer.push(key);
-    return answer;
-}
-
-// 6. 가장 큰 수
-function solution6(numbers) {
-    var answer = '';
-    let temp = [];
-    for(let i = 0; i < numbers.length; ++i) temp.push(numbers[i].toString());
-    temp.sort().reverse();
-    for(let i = 0; i < temp.length - 1; ++ i){
-        let nextSize = temp[i + 1].length;
-        if(temp[i].slice(0, nextSize) === temp[i+1] && temp[i].length > temp[i + 1].length){
-            if(temp[i][nextSize] <= temp[i+1][0] && temp[i + 1][nextSize - 1] !== '0'){
-                let t = temp[i];
-                temp[i] = temp[i+1];
-                temp[i+1] = t;         
-            }
-        }
-    }
-    
-    for(let val of temp){
-        if(val[0] !== '0') answer += val;
-    }
-    if(answer === "") answer = '0';
     return answer;
 }
---
+++ week5/byeongwan/byeongwan.js
@@ -0,0 +1,58 @@
+// 피보나치 수
+function solution(n) {
+    let f = new Array(n + 1);
+    f[0] = 0; f[1] = 1;
+    for(let i = 2; i <= n; ++i) f[i] = (f[i - 1] + f[i - 2]) % 1234567;    
+    return f[n];
+}
+
+// 소수 찾기
+// 1. 소수 판별 function
+// 2. 길이가 1부터 n까지 숫자 조합 function
+function solution(numbers) {
+    var answer = 0;    
+    let joinResult = [];
+
+    // numbers 데이터를 순열로 변환
+    for(let i = 0; i < numbers.length; ++i){
+        let splitResult =permute([], [], numbers.split(""), i)
+        for(let j = 0; j < splitResult.length; ++j)
+            joinResult.push(parseInt(splitResult[j].join("")));
+    }    
+    // 중복 제거 후 변환한 데이터 마다 소수 판별
+    joinResult.filter((item, index) => joinResult.indexOf(item) === index).forEach(e => {
+        if(isPrime(e)) ++answer;
+    })
+    
+    return answer;
+}
+
+console.log(solution("011"));
+
+// 순열
+function permute(permArr,usedChars, input, r) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == r) {
+      permArr.push(usedChars.slice());
+    }
+    permute(permArr, usedChars, input, r);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr
+};
+
+// 소수
+function isPrime(num){
+    if(num === 2) return true;
+    if(num % 2 === 0 || num === 1) return false;
+
+    let root = Math.sqrt(num);
+    for(let i = 3; i <= root; i += 2){
+        if(num % i === 0) return false;
+    }
+    return true;
+}
