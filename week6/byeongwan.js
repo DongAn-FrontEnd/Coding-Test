0b64869eda97baf280f751725b52d05dc3e5628e
ChangHyun2 <56799176+ChangHyun2@users.noreply.github.com>
2020-06-20 07:51:14 +0900

    Week6 (#24)
    * week6 init
    
    * week6 창현 [Done] 다음 큰 숫자
    
    * week6 창현[Doing] 땅따먹기
    
    * week6 다음 큰 숫자, 위장, 구명보트, 땅따먹기
    
    * week6-byeongwan [Done] 1, 2
    
    * weeek6-byeongwan [Done] 4]
    
    * week6-byeongwan [Done] leetcode 1, 2
    
    * refactor
    
    Co-authored-by: garosero <gangya250@gmail.com>
    Co-authored-by: KimSatGat <cbaal@korea.com>

---
+++ week6/byeongwan.js
@@ -0,0 +1,91 @@
+// 다음 큰 숫자
+function solution(n) {
+    let nCount = countOne(decimalToBinary(n));
+    while(true){
+      ++n;
+      let biggerNumCount = countOne(decimalToBinary(n));
+      if(nCount === biggerNumCount) return n;
+    }  
+  }
+  
+  function decimalToBinary(data){
+    let bin = "";
+    while(data !== 0){
+      let remainder = data % 2;
+      data = parseInt(data / 2);
+      bin = remainder.toString()+ bin;
+    }
+    return bin;
+  }
+  
+  function countOne(data){
+    let count = 0;
+    for(let i = 0; i < data.length; ++i)
+      if(data[i] === "1") ++count;      
+    return count;
+  }
+
+// 위장
+function solution(clothes) {
+    var answer = 1;
+    
+    var hash = new Map();
+    
+    for(var i = 0; i < clothes.length; ++i){
+        var value = hash.get(clothes[i][1]);
+        // value가 있다면 1증가하여 set
+        if(value !== undefined){
+            ++value;
+            hash.set(clothes[i][1], value);
+        }
+        // value가 있다면 1로 set
+        else{            
+            hash.set(clothes[i][1], 1);
+        }
+    }
+    // values를 가지고와서 반복
+    var it = hash.values();
+    for(var i = 0; i < hash.size; ++i){
+        answer *= (it.next().value + 1);    // 안입는 경우도 추가하여 +1
+    }
+    
+    answer -= 1;    // 아무것도 안입는 경우를 뺀다 -1
+    return answer;
+}
+
+// 땅따먹기 - Fail
+
+// 구명보트
+function solution(people, limit) {
+    var boat = 0;
+    let front = 0;
+    let back = people.length - 1;
+    people.sort((a,b) => b - a);
+    while(front <= back){
+        if(people[front] + people[back] <= limit)
+            --back;        
+        ++front;
+        ++boat;
+    }
+    return boat;
+}
+
+// 1480	Running Sum of 1d Array
+var runningSum = function(nums) {
+  let output = new Array(nums.length).fill(0);
+  output[0] = nums[0];
+  for(let i = 1; i < output.length; ++i){
+    output[i] += output[i - 1] + nums[i];
+  }
+  return(output);
+};
+
+// 1470. Shuffle the Array
+var shuffle = function(nums, n) {
+  let answer = [];
+  for(let i = 0; i < n; ++i){
+    answer.push(nums[i]);
+    answer.push(nums[i + n]);
+  }
+  return answer;
+};
